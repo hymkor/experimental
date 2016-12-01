4e5e41e7a030d4d40576ecdf613d5725081e0ecf
HAYAMA_Kaoru <iyahaya@nifty.com>
2016-12-02 01:12:17 +0900

    Update searchlst(completed?)

--- searchlst/searchlst.go
+++ searchlst/searchlst.go
@@ -4,19 +4,14 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
 )
 
 func main() {
 	filepath.Walk(".", func(path1 string, info os.FileInfo, err error) error {
-		if info.IsDir() {
+		if ! info.IsDir() {
 			return nil
 		}
-		path1lower := strings.ToLower(path1)
-		if !strings.HasSuffix(path1lower, ".dwg") {
-			return nil
-		}
-		fmt.Println(path1[:len(path1)-4])
+		fmt.Println(path1[:len(path1)-4] + `\`)
 		return nil
 	})
 }
