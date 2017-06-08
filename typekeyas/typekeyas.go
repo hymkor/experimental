03664ec4bae9365f4f7b008a8ee3a7e45f14ab69
Kaoru HAYAMA <iyahaya@nifty.com>
2017-06-08 11:16:23 +0900

    move package consoleinput to github.com/zetamatta/go-getch/consoleinput

--- typekeyas/typekeyas.go
+++ typekeyas/typekeyas.go
@@ -3,17 +3,38 @@ package main
 import (
 	"os"
 
-	"github.com/zetamatta/experimental/typekeyas/consoleinput"
+	"github.com/zetamatta/go-getch/consoleinput"
 )
 
+func writeRune(handle consoleinput.Handle,c rune) uint32 {
+	records := []consoleinput.InputRecord{
+		consoleinput.InputRecord{EventType: consoleinput.KEY_EVENT},
+		consoleinput.InputRecord{EventType: consoleinput.KEY_EVENT},
+	}
+	keydown := records[0].KeyEvent()
+	keydown.KeyDown = 1
+	keydown.UnicodeChar = uint16(c)
+
+	keyup := records[1].KeyEvent()
+	keyup.UnicodeChar = uint16(c)
+
+	return handle.Write(records[:])
+}
+
+func  writeString(handle consoleinput.Handle,s string) {
+	for _, c := range s {
+		writeRune(handle,c)
+	}
+}
+
 func main() {
-	console, err := consoleinput.NewHandle()
+	console, err := consoleinput.New()
 	if err != nil {
 		println(err.Error())
 		return
 	}
 	for _, s := range os.Args[1:] {
-		console.WriteString(s)
-		console.WriteRune('\r')
+		writeString(console,s)
+		writeRune(console,'\r')
 	}
 }
