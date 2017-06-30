4aac5657094ac5a37ae00b0fe6c339475e90b81d
HAYAMA_Kaoru <iyahaya@nifty.com>
2017-06-30 13:54:04 +0900

    Test code for VB_Net's CStr(Nothing) function

---
+++ TestCStr/TestCStr/Module1.vb
@@ -0,0 +1,9 @@
+﻿Module Module1
+    Sub Main()
+        Dim foo As String = Nothing
+        Dim bar As String = CStr(foo)
+
+        Console.Write("CStr(Nothing)={0}", If(bar, "(null)"))
+        Console.ReadLine()
+    End Sub
+End Module
