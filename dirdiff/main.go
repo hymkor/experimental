a7472a830c57abae768d09bb9db122a0d0a4ede5
HAYAMA_Kaoru <iyahaya@nifty.com>
2018-01-30 17:13:53 +0900

    dirdiff: -i option

--- dirdiff/main.go
+++ dirdiff/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/md5"
+	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -9,6 +10,8 @@ import (
 	"path/filepath"
 	"strings"
 )
+
+var option_i = flag.String("i", "*", "compared files pattern")
 
 func GetFileHash(fname string) (string, error) {
 	data, err := ioutil.ReadFile(fname)
@@ -31,30 +34,41 @@ func GetDirHash(dir string) (map[string]string, error) {
 	}
 	dict := map[string]string{}
 	for _, file1 := range files {
-		if !file1.IsDir() {
-			path1 := filepath.Join(dir, file1.Name())
-			hash1, err := GetFileHash(path1)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "%s: %s\n", path1, err.Error())
-			} else {
-				dict[strings.ToUpper(file1.Name())] = hash1
-			}
+		if file1.IsDir() {
+			continue
+		}
+		matched, err := filepath.Match(
+			strings.ToUpper(*option_i),
+			strings.ToUpper(file1.Name()))
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			continue
+		}
+		if !matched {
+			continue
+		}
+		path1 := filepath.Join(dir, file1.Name())
+		hash1, err := GetFileHash(path1)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s: %s\n", path1, err.Error())
+		} else {
+			dict[strings.ToUpper(file1.Name())] = hash1
 		}
 	}
 	return dict, nil
 }
 
-func main1() error {
-	if len(os.Args) < 3 {
+func main1(args []string) error {
+	if len(args) < 2 {
 		return fmt.Errorf("Usage: dirdiff DIR1 DIR2")
 	}
-	dirhash1, err := GetDirHash(os.Args[1])
+	dirhash1, err := GetDirHash(args[0])
 	if err != nil {
-		return fmt.Errorf("%s: %s", os.Args[1], err.Error())
+		return fmt.Errorf("%s: %s", args[0], err.Error())
 	}
-	dirhash2, err := GetDirHash(os.Args[2])
+	dirhash2, err := GetDirHash(args[1])
 	if err != nil {
-		return fmt.Errorf("%s: %s\n", os.Args[2], err.Error())
+		return fmt.Errorf("%s: %s\n", args[1], err.Error())
 	}
 
 	for name, hash1 := range dirhash1 {
@@ -63,19 +77,20 @@ func main1() error {
 				fmt.Printf("%s: differs\n", name)
 			}
 		} else {
-			fmt.Printf("%s: not found in %s\n", name, os.Args[2])
+			fmt.Printf("%s: not found in %s\n", name, args[1])
 		}
 	}
 	for name, _ := range dirhash2 {
 		if _, ok := dirhash1[name]; !ok {
-			fmt.Printf("%s: not found in %s\n", name, os.Args[1])
+			fmt.Printf("%s: not found in %s\n", name, args[0])
 		}
 	}
 	return nil
 }
 
 func main() {
-	if err := main1(); err != nil {
+	flag.Parse()
+	if err := main1(flag.Args()); err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	} else {
