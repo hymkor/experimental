09c4e77dd3c76b7f73a4d639f5390e2af7069bc4
Kaoru HAYAMA <iyahaya@nifty.com>
2018-06-10 20:41:17 +0900

    goShowVer: font big and scroll-bar

--- goShowVer/main.go
+++ goShowVer/main.go
@@ -1,31 +1,43 @@
 package main
 
 import (
-	"github.com/lxn/walk"
-	. "github.com/lxn/walk/declarative"
+	"os"
 	"strings"
+
+	"github.com/lxn/walk"
+	W "github.com/lxn/walk/declarative"
 )
 
-func main() {
-	var inTE, outTE *walk.TextEdit
+func ShowSimpleOneWindow(text string) error {
+	var form1 *walk.MainWindow
+	var te *walk.TextEdit
 
-	MainWindow{
-		Title:   "SCREAMO",
-		MinSize: Size{600, 400},
-		Layout:  VBox{},
-		Children: []Widget{
-			HSplitter{
-				Children: []Widget{
-					TextEdit{AssignTo: &inTE},
-					TextEdit{AssignTo: &outTE, ReadOnly: true},
-				},
-			},
-			PushButton{
-				Text: "SCREAM",
+	W.MainWindow{
+		Title:    "goShowVer",
+		AssignTo: &form1,
+		MinSize:  W.Size{640, 200},
+		Layout:   W.VBox{},
+		Children: []W.Widget{
+			W.TextEdit{
+				AssignTo: &te,
+				Text:     text,
+				Font:     W.Font{PointSize: 12},
+				VScroll: true,
+				ReadOnly: false},
+			W.PushButton{
+				Text: "Ok",
 				OnClicked: func() {
-					outTE.SetText(strings.ToUpper(inTE.Text()))
+					form1.Close()
 				},
 			},
 		},
 	}.Run()
+	return nil
+}
+
+func main() {
+	err := ShowSimpleOneWindow(strings.Join(os.Args, " "))
+	if err != nil {
+		println(err.Error())
+	}
 }
