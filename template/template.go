13c51442423c7d4c297a6a6bc6c9d625fc903ace
HAYAMA_Kaoru <iyahaya@nifty.com>
2018-07-09 18:27:07 +0900

    Add template

---
+++ template/template.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	// "flag"
+	"fmt"
+	"os"
+)
+
+func main1(args []string) error {
+	return nil
+}
+
+func main() {
+	// flag.Parse() // use `flag.Args()` rather than `os.Args[1:]`
+	if err := main1(os.Args[1:]); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
