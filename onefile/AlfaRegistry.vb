147326058432bba1be04b36f0d27a03620113841
HAYAMA_Kaoru <iyahaya@nifty.com>
2018-07-13 16:49:32 +0900

    onefile/AlfaRegistry.vb : DWORD対応、フォームの位置記憶機能対応

--- onefile/AlfaRegistry.vb
+++ onefile/AlfaRegistry.vb
@@ -19,7 +19,7 @@ Public Class AlfaRegistry
             If IsNothing(Reg) Then
                 Me.Reg = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(Me.Folder)
             End If
-            Return CType(If(Me.Reg IsNot Nothing, Me.Reg.GetValue(key, Microsoft.Win32.RegistryValueKind.String), Nothing), String)
+            Return CType(If(Me.Reg IsNot Nothing, Me.Reg.GetValue(key, Nothing), Nothing), String)
         End Get
         Set(ByVal value As String)
             If IsNothing(Reg) Then
@@ -29,12 +29,62 @@ Public Class AlfaRegistry
         End Set
     End Property
 
+    Public Property DWord(key As String) As Integer
+        Get
+            If Reg Is Nothing Then
+                Me.Reg = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(Me.Folder)
+            End If
+            Return CInt(If(Me.Reg IsNot Nothing, Me.Reg.GetValue(key, -1), -1))
+        End Get
+        Set(value As Integer)
+            If Me.Reg Is Nothing Then
+                Me.Reg = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(Me.Folder)
+            End If
+            Me.Reg.SetValue(key, value, Microsoft.Win32.RegistryValueKind.DWord)
+        End Set
+    End Property
+
     Public Sub Close()
         If Not IsNothing(Me.Reg) Then
             Me.Reg.Close()
             Me.Reg = Nothing
         End If
     End Sub
+
+    Public Sub LoadPos(form1 As System.Windows.Forms.Form)
+        Dim name = form1.Name
+        Dim x = Me.DWord(name & "X")
+        If x < 0 Then Return
+
+        Dim y = Me.DWord(name & "Y")
+        If y < 0 Then Return
+
+        Dim w = Me.DWord(name & "W")
+        If w < 0 Then Return
+
+        Dim h = Me.DWord(name & "H")
+        If h < 0 Then Return
+
+        Dim b = System.Windows.Forms.Screen.PrimaryScreen.Bounds
+        If x < b.Left OrElse y < b.Top OrElse
+            x + w > b.Right OrElse y + h > b.Bottom Then Return
+
+        form1.Location = New System.Drawing.Point(x, y)
+        form1.Size = New System.Drawing.Size(w, h)
+    End Sub
+
+    Public Sub SavePos(form1 As System.Windows.Forms.Form)
+        Dim loc = form1.Location
+        Dim siz = form1.Size
+        Dim name = form1.Name
+        Me.DWord(name & "X") = loc.X
+        Me.DWord(name & "Y") = loc.Y
+        Me.DWord(name & "W") = siz.Width
+        Me.DWord(name & "H") = siz.Height
+    End Sub
+
+
+
 
 #Region "IDisposable Support"
     Private disposedValue As Boolean ' 重複する呼び出しを検出するには
