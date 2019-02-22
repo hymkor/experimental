194bcd78d0d3b887129ccf0eda1d804fae394c59
HAYAMA_Kaoru <iyahaya@nifty.com>
2019-02-23 00:05:19 +0900

    searchlst: output by cp932

--- searchlst/searchlst.go
+++ searchlst/searchlst.go
@@ -4,9 +4,21 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+
+	"golang.org/x/text/encoding/japanese"
 )
 
-func main() {
+const fname = "search.lst"
+
+func main1() error {
+	fd, err := os.OpenFile(fname, os.O_CREATE|os.O_EXCL, 0666)
+	if err != nil {
+		return err
+	}
+	defer fd.Close()
+
+	w := japanese.ShiftJIS.NewEncoder().Writer(fd)
+
 	filepath.Walk(".", func(path1 string, info os.FileInfo, err error) error {
 		if !info.IsDir() {
 			return nil
@@ -14,7 +26,15 @@ func main() {
 		if matched, err := filepath.Match(path1, "."); err != nil || matched {
 			return nil
 		}
-		fmt.Println(path1 + `\`)
+		fmt.Fprintln(w, path1+`\`)
 		return nil
 	})
+	return nil
+}
+
+func main() {
+	if err := main1(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
